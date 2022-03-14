ee08c4f19f5565bddf66e2f3e993d809efd70669
Rafael Silva <rafa.daitx.silva@rede.ulbra.br>
2022-03-14 00:06:17 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError 401

--- test/data/usecases/remote_authencation_test.dart
+++ test/data/usecases/remote_authencation_test.dart
@@ -54,6 +54,34 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenThrow(HttpError.serverError);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw InvaliudCredentialsError if HttpClient returns 501', () async {
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenThrow(HttpError.unauthorized);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
+  });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
