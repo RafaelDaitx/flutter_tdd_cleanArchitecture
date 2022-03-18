0672130fbbd6a2c37b3ce779e693f5363a060793
Rafael Silva <rafa.daitx.silva@rede.ulbra.br>
2022-03-18 01:27:19 -0300

    ensure HttpAdapter throws ForbiddenError on  403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -104,6 +104,14 @@ void main() {
       expect(future, throwsA(HttpError.unauthorized));
     });
 
+    test('Should return ForbiddenError if post returns 403', () async {
+      mockResponse(403, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
+    });
+
     test('Should return ServerError if post returns 500', () async {
       mockResponse(400);
 
