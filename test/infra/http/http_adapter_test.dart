51c82f28709d4e747bb1529fc49670d590534f80
Rafael Silva <rafa.daitx.silva@rede.ulbra.br>
2022-03-18 01:41:59 -0300

    ensure HttpAdapterthrows ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -31,6 +31,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -129,11 +133,19 @@ void main() {
     });
 
     test('Should return ServerError if post returns 500', () async {
-      mockResponse(400);
+      mockResponse(500);
 
       final future = sut.request(url: url, method: 'post');
 
-      expect(future, throwsA(HttpError.badRequest));
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
