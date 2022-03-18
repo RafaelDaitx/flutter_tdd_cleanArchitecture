cd41a15cd61770ef90d8ff15216f2003fc47f9b8
Rafael Silva <rafa.daitx.silva@rede.ulbra.br>
2022-03-18 00:49:06 -0300

    move HttpAdapter to its own file

---
+++ lib/data/infra/http/http_adapter.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+
+import '../../http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({
+    @required String url,
+    @required String method,
+    Map body,
+  }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final jsinBody = body != null ? jsonEncode(body) : null;
+    final response = await client.post(url, headers: headers, body: jsinBody);
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    }
+    return null;
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,12 +1,8 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
-import 'package:udemy_cervantes/data/http/http.dart';
+import 'package:udemy_cervantes/data/infra/http/http.dart';
 
 void main() {
   ClientSpy client;
@@ -86,26 +82,3 @@ void main() {
 }
 
 class ClientSpy extends Mock implements Client {}
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body,
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-    final jsinBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(url, headers: headers, body: jsinBody);
-    if (response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    }
-    return null;
-  }
-}
